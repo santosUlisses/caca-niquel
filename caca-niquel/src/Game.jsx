00873b8a0348107ba600b37b11189975f8442ae2
Ulisses <ulisses.gc@hotmail.com>
2024-01-23 22:46:24 -0300

    adicionando imagem

--- caca-niquel/src/Game.jsx
+++ caca-niquel/src/Game.jsx
@@ -5,7 +5,7 @@ import { IoMdRemove, IoIosAdd } from "react-icons/io";
 
 export const Game = () => {
 
-    const emogi = [<BiSolidTrophy color='gold' />, <BiLogoReact className='reacticone' color='orange' />, <GiAk47U color='grey' />, <GiAnglerFish color='blue' />, <GiFullMotorcycleHelmet color='red' />, <GiHemp color='green' />];
+    const emogi = [<BiSolidTrophy color='gold' />, <BiLogoReact className='reacticone' color='orange' />, <GiAk47U color='grey' />, <GiFullMotorcycleHelmet color='red' />, <GiAnglerFish color='blue' />, <GiHemp color='green' />];
     let contEmogi = emogi.length;
 
     const [saldo, setSaldo] = useState(20000);
@@ -23,15 +23,15 @@ export const Game = () => {
         setAposta(aposta + 5);
     }
 
-    const [n, setN] = useState(4);
-    const [c, setC] = useState(4);
-    const [d, setD] = useState(4);
-    const [e, setE] = useState(4);
-    const [f, setF] = useState(4);
-    const [g, setG] = useState(4);
-    const [h, setH] = useState(4);
-    const [i, setI] = useState(4);
-    const [j, setJ] = useState(4);
+    const [n, setN] = useState(5);
+    const [c, setC] = useState(5);
+    const [d, setD] = useState(5);
+    const [e, setE] = useState(2);
+    const [f, setF] = useState(2);
+    const [g, setG] = useState(2);
+    const [h, setH] = useState(3);
+    const [i, setI] = useState(3);
+    const [j, setJ] = useState(3);
 
     const [exibirLinha, setExibirLinha] = useState(false);
     const [exibirLinha2, setExibirLinha2] = useState(false);
@@ -41,6 +41,60 @@ export const Game = () => {
     const [exibirLinha6, setExibirLinha6] = useState(false);
     const [mensagem, setMensagem] = useState(false);
     const [erro, setErro] = useState(false);
+
+
+    const exibirGanho = (a, b, c) => {
+
+        if (a === 0 && b === 0 && c === 0) {
+            setSaldo((prevSaldo) => {
+                const novoGanho = aposta * 5000;
+                prevSaldo + aposta * 5000;
+                setGanho(ganho + novoGanho);
+                return saldo + novoGanho
+            });
+        }
+        else if (a === 1 && b === 1 && c === 1) {
+            setSaldo((prevSaldo) => {
+                const novoGanho = aposta * 1000;
+                prevSaldo + aposta * 1000;
+                setGanho(novoGanho);
+                return saldo + novoGanho
+            });
+        }
+        else if (a === 2 && b === 2 && c === 2) {
+            setSaldo((prevSaldo) => {
+                const novoGanho = aposta * 500;
+                prevSaldo + aposta * 500;
+                setGanho(novoGanho);
+                return saldo + novoGanho
+            });
+        }
+        else if (a === 3 && b === 3 && c === 3) {
+            setSaldo((prevSaldo) => {
+                const novoGanho = aposta * 250;
+                prevSaldo + aposta * 250;
+                setGanho(novoGanho);
+                return saldo + novoGanho
+            });
+        }
+        else if (a === 4 && b === 4 && c === 4) {
+            setSaldo((prevSaldo) => {
+                const novoGanho = aposta * 100;
+                prevSaldo + aposta * 100;
+                setGanho(novoGanho);
+                return saldo + novoGanho
+            });
+        }
+        else if (a === 5 && b === 5 && c === 5) {
+            setSaldo((prevSaldo) => {
+                const novoGanho = aposta * 50;
+                prevSaldo + aposta * 50;
+                setGanho(novoGanho);
+                return saldo + novoGanho
+            });
+        }
+
+    }
 
     const gerar = () => {
         if (saldo >= aposta) {
@@ -65,63 +119,16 @@ export const Game = () => {
             return clearMsg;
         }
 
-
-        if (n === 0 && c === 0 && d === 0) {
-            setSaldo((prevSaldo) => {
-                const novoGanho = aposta * 5000;
-                prevSaldo + aposta * 5000;
-                setGanho(ganho + novoGanho);
-                return saldo + novoGanho
-            });
-        }
-        else if (n === 1 && c === 1 && d === 1) {
-            setSaldo((prevSaldo) => {
-                const novoGanho = aposta * 1000;
-                prevSaldo + aposta * 1000;
-                setGanho(novoGanho);
-                return saldo + novoGanho
-            });
-        }
-        else if (n === 2 && c === 2 && d === 2) {
-            setSaldo((prevSaldo) => {
-                const novoGanho = aposta * 500;
-                prevSaldo + aposta * 500;
-                setGanho(novoGanho);
-                return saldo + novoGanho
-            });
-        }
-        else if (n === 3 && c === 3 && d === 3) {
-            setSaldo((prevSaldo) => {
-                const novoGanho = aposta * 250;
-                prevSaldo + aposta * 250;
-                setGanho(novoGanho);
-                return saldo + novoGanho
-            });
-        }
-        else if (n === 4 && c === 4 && d === 4) {
-            setSaldo((prevSaldo) => {
-                const novoGanho = aposta * 100;
-                prevSaldo + aposta * 100;
-                setGanho(novoGanho);
-                return saldo + novoGanho
-            });
-        }
-        else if (n === 5 && c === 5 && d === 5) {
-            setSaldo((prevSaldo) => {
-                const novoGanho = aposta * 50;
-                prevSaldo + aposta * 50;
-                setGanho(novoGanho);
-                return saldo + novoGanho
-            });
-        }
     }
 
 
     useEffect(() => {
         if (n === c && c === d) {
-            
+
             setExibirLinha(true);
             setMensagem(true);
+
+            exibirGanho(n, c, d);
 
             const linha = setTimeout(() => {
                 setExibirLinha(false);
@@ -133,15 +140,16 @@ export const Game = () => {
                 clearTimeout(linha);
             };
         }
-    }, [n, c, d]);
+    }, [n, c, d, e, f, g, h, i, j]);
 
 
     useEffect(() => {
         if (e === f && f === g) {
-            
+
             setExibirLinha2(true);
             setMensagem(true);
-
+            
+            exibirGanho(e,f,g);
 
             const linha = setTimeout(() => {
                 setExibirLinha2(false);
@@ -151,14 +159,17 @@ export const Game = () => {
             return () => {
                 clearTimeout(linha);
             };
+
         }
-    }, [e, f, g]);
+    }, [n, c, d, e, f, g, h, i, j]);
 
     useEffect(() => {
         if (h === i && i === j) {
-            
+
             setExibirLinha3(true);
             setMensagem(true);
+
+            exibirGanho(h,i,j);
 
             const linha = setTimeout(() => {
                 setExibirLinha3(false);
@@ -169,14 +180,16 @@ export const Game = () => {
                 clearTimeout(linha);
             };
         }
-    }, [h, i, j]);
+    }, [n, c, d, e, f, g, h, i, j]);
 
 
     useEffect(() => {
         if (n === e && e === h) {
-            
+
             setExibirLinha4(true);
             setMensagem(true);
+
+           exibirGanho(n,e,h);
 
             const linha = setTimeout(() => {
                 setExibirLinha4(false);
@@ -187,14 +200,16 @@ export const Game = () => {
                 clearTimeout(linha);
             };
         }
-    }, [e, n, h]);
+    }, [n, c, d, e, f, g, h, i, j]);
 
 
     useEffect(() => {
         if (c === f && f === i) {
-            
+
             setExibirLinha5(true);
             setMensagem(true);
+
+            exibirGanho(c,f,i);
 
             const linha = setTimeout(() => {
                 setExibirLinha5(false);
@@ -205,14 +220,16 @@ export const Game = () => {
                 clearTimeout(linha);
             };
         }
-    }, [c, f, i]);
+    }, [n, c, d, e, f, g, h, i, j]);
 
 
     useEffect(() => {
         if (d === g && g === j) {
-            
+
             setExibirLinha6(true);
             setMensagem(true);
+
+            exibirGanho(d,g,j);
 
             const linha = setTimeout(() => {
                 setExibirLinha6(false);
@@ -223,7 +240,7 @@ export const Game = () => {
                 clearTimeout(linha);
             };
         }
-    }, [d, g, j]);
+    }, [n, c, d, e, f, g, h, i, j]);
 
 
 
@@ -231,7 +248,11 @@ export const Game = () => {
     return (
         <div className='container'>
 
-            <p id='p'>DIVIRTA-SE</p>
+
+            <h2 className='boaSorte' id='p'>BOA SORTE</h2>
+
+
+
             {mensagem && <div className="mensagem"><p>PARABÉNS VOCÊ GANHOU R$ {ganho}</p></div>}
             {erro && <div className='error'><p>SALDO INSUFICIENTE</p></div>}
 
@@ -271,22 +292,19 @@ export const Game = () => {
 
 
             <div className="premios">
+
                 <div className="box">
                     <div className="icones1">
-                    <p><BiSolidTrophy color='gold' />  x5000</p>
-                    <p><BiLogoReact className='reacticone' color='orange' /> x1000</p>
-                    <p><GiAk47U color='grey' />  x500</p>
-                    </div>
-
-
-                    <div className="icones1">
-                    <p><GiFullMotorcycleHelmet color='red' />  x250</p>
-                    <p><GiAnglerFish color='blue'/> x100</p>
-                    <p><GiHemp color='green' />  x50</p>
+                        <p><BiSolidTrophy color='gold' />  x5000</p>
+                        <p><BiLogoReact className='reacticone' color='orange' /> x1000</p>
+                        <p><GiAk47U color='grey' />  x500</p>
+                        <p><GiFullMotorcycleHelmet color='red' />  x250</p>
+                        <p><GiAnglerFish color='blue' /> x100</p>
+                        <p><GiHemp color='green' />  x50</p>
                     </div>
                 </div>
             </div>
-
+            <h3 className='listaPremios'>Lista de prêmios</h3>
         </div>
 
 
